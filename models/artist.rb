abdcc2617e75b27d5c96b393dc325128001d4cc3
lindsey <lj0978@hotmail.co.uk>
2019-04-03 18:45:49 +0100

    added update artist

--- models/artist.rb
+++ models/artist.rb
@@ -35,4 +35,12 @@ class Artist
     results = SqlRunner.run(sql, values)
     return results.map {|album| Album.new(album)}
   end
+
+  def update()
+    sql = "UPDATE artist SET (artist_name) = ($1) WHERE id = $2"
+    values = [@artist_name, @id]
+    SqlRunner.run(sql, values)
+  end  
+
+
 end
